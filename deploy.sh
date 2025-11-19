a2f65d4630adc23ed297883019d51b14e9eec8f7
Victor <vtorPss@gmail.com>
2025-11-19 10:02:46 +0700

    first commit

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Deploy the application using Docker Compose
+docker compose up --build
---
+++ stop-service.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Stop the application using Docker Compose
+docker compose down
